7c9fe99c0bfec6f4a14e3590205da13bcd4b70c6
Andrew Lyu <duchaolv@gmail.com>
2017-03-08 21:31:54 -0600

    temperary update from another computer

--- notebook/feature_select.py
+++ notebook/feature_select.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.linear_model import RandomizedLasso
@@ -15,7 +16,8 @@ class Feature_Filter:
         self.y = y
         self.names = X_train.columns
         self.par_grid = par_grid
-        self.rankdict = {}
+        #self.rankdict = {}
+        self.ranks_df = pd.DataFrame()
         
     def tune_par_grid(self):
         grid = GridSearchCV(
@@ -38,11 +40,14 @@ class Feature_Filter:
         #print(rsearch.best_estimator_.alpha)
         self.SearchCV = rsearch    
     
-    def rank_to_dict(self, ranks, names, order=1):
+    def rank_to_dict(self, rank, names, order=1):
         minmax = MinMaxScaler()
-        ranks = minmax.fit_transform(order*np.array([ranks]).T).T[0]
-        ranks = map(lambda x: round(x, 2), ranks)
-        return dict(zip(names, ranks ))
+        rank = minmax.fit_transform(order*np.array([rank]).T).T[0]
+        rank = map(lambda x: round(x, 2), rank)
+        rank_dict = dict(zip(names, rank ))
+        rank_dict = [(k, rank_dict[k]) for k in 
+                     sorted(rank_dict, key=rank_dict.get, reverse=True)]
+        return rank_dict
 
     def rand_lasso(self,max_it=50000, n_resample = 500):
         par = self.SearchCV.best_estimator_.alpha
@@ -51,9 +56,16 @@ class Feature_Filter:
                 random_state=1,
                 n_resampling=n_resample)
         rlasso.fit(self.x, self.y)
-        self.rankdict["Rn_La"] = self.rank_to_dict(np.abs(rlasso.scores_), self.names)
+        # create a dict to store the ranks 
+        #rankdict={}
+        #rankdict["Rn_La"] = self.rank_to_dict(np.abs(rlasso.scores_), self.names)
+        #self.ranks_df = pd.DataFrame.from_dict(rankdict)
+        #self.ranks_df = self.ranks_df.append(pd.DataFrame(self.rankdict))
+        return rlasso.scores_
         
     def auto_select(self,N_searchCV=100):
         np.random.seed(1)
         self.tune_par_rand(N_search=N_searchCV)
-        self.rand_lasso()
+        scores = self.rand_lasso()
+        return scores
+        
